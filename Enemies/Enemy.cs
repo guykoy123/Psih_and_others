d3dec109f0e8ec881f7bac1e92826117291b7c76
guykoy123 <23092757+guykoy123@users.noreply.github.com>
2019-09-15 20:21:28 +0300

    added enemy heal delay

--- Enemies/Enemy.cs
+++ Enemies/Enemy.cs
@@ -12,8 +12,14 @@ public class Enemy : MonoBehaviour{
     private float jump_force;
     private string enemy_name;
 
+    private bool Can_Heal = false; //stores if the enemy can heal
+    private float Last_Heal_Time=0f; //strores the last time enemy healed
+    private float Heal_Delay = 3f; //time in seconds between each heal
+
     private GameObject Enemy_UI; //stores the Enemy_UI object (has all the UI elemnets as children)
     private GameObject Player_Camera; //stores the player camera (to update rotation of UI)
+
+    
 
     private void Start()
     {
@@ -25,8 +31,11 @@ public class Enemy : MonoBehaviour{
         Enemy_UI.transform.LookAt(Player_Camera.transform); //rotate Enemy_UI to face player camera
         Enemy_UI.transform.Rotate(new Vector3(0, 180, 0)); //rotate Enemy_UI so text will not be backwards
 
-        string text_box = enemy_name + "\n" + "Health:" + current_health + "/" + base_health; //generate text for the text box (name, health info)
+        string text_box = enemy_name + "\n" + "Health:" + (int)current_health + "/" + (int)base_health; //generate text for the text box (name, health info [as whole numbers])
         Enemy_UI.GetComponentInChildren<TextMesh>().text = text_box; //update textbox
+
+        if (Time.time - Last_Heal_Time >= Heal_Delay) //if enough time has passed since healing
+            Can_Heal = true; //enemy can heal
     }
 
     public bool Hit(float damage) 
@@ -38,12 +47,13 @@ public class Enemy : MonoBehaviour{
         if (amount>0f) //check that amount is positive (defense does not exceed damage)
         {
             current_health -= amount; //reduce amount from current health
+
         }
 
         if (current_health > 0)//check if enemy has health
         {
             Debug.Log(enemy_name + " health: " + current_health);
-            return true;
+            return true; //return true because enemy is alive
         }
             
         else
@@ -53,17 +63,20 @@ public class Enemy : MonoBehaviour{
         }
     } 
 
-    public void Heal(float amount) 
+    public bool Heal(float amount) 
     {
         //heals enemy
         //does not exceed the base health
 
-        if(current_health < base_health) //check if current health is lower than base health (maximum health)
+        if(current_health < base_health && Can_Heal) //check if current health is lower than base health (maximum health)
         {
             current_health += amount; //add healing amount to current health
             current_health = Mathf.Clamp(current_health, 0f, base_health); //clamp current health to not exceed base health
             Debug.Log(name + " healed: " + amount);
-        }    
+            Can_Heal = false; //because enemy just healed, set can_heal to false
+            Last_Heal_Time = Time.time; //update last healing time
+        }
+        return Can_Heal;
     }
 
     //get/set function for all variables
@@ -81,5 +94,7 @@ public class Enemy : MonoBehaviour{
     public void Set_Jump_Force(float f) { jump_force = Mathf.Max(0f, f); } //set jump force (minimum value is 0)
     public string Get_Name() { return enemy_name; }//return enemy name
     public void Set_Name(string n) { enemy_name = n; } //set enemy name
+    public void Set_Heal_Delay(float delay) { Heal_Delay = delay; } //set heal delay time
+    public float Get_Heal_Delay() { return Heal_Delay; }//return enemy heal delay time
 
 }
